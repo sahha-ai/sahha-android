8ab17f5608e3cd94f94589354f03ef043e578098
Min <128448100+min-sahha@users.noreply.github.com>
2024-05-21 10:08:47 +1200

    fix reason int mappings being incorrect

--- sahha-android/src/main/java/sdk/sahha/android/domain/model/error_log/AppCrashInfo.kt
+++ sahha-android/src/main/java/sdk/sahha/android/domain/model/error_log/AppCrashInfo.kt
@@ -12,6 +12,7 @@ internal data class AppCrashInfo(
 
 @RequiresApi(Build.VERSION_CODES.R)
 internal fun ApplicationExitInfo.toAppCrashInfo(): AppCrashInfo {
+    ApplicationExitInfo.REASON_EXIT_SELF
     return AppCrashInfo(
         reason = this.reason,
         description = this.description ?: "Something went wrong",
@@ -23,20 +24,20 @@ internal enum class CrashReason(private val reason: Int) {
     REASON_ANR(6),
     REASON_CRASH(4),
     REASON_CRASH_NATIVE(5),
-    REASON_DEPENDENCY_DIED(2),
+    REASON_DEPENDENCY_DIED(12),
     REASON_EXCESSIVE_RESOURCE_USAGE(9),
     REASON_EXIT_SELF(1),
-    REASON_FREEZER(4),
+    REASON_FREEZER(14),
     REASON_INITIALIZATION_FAILURE(7),
     REASON_LOW_MEMORY(3),
-    REASON_OTHER(3),
-    REASON_PACKAGE_STATE_CHANGE(5),
-    REASON_PACKAGE_UPDATED(6),
+    REASON_OTHER(13),
+    REASON_PACKAGE_STATE_CHANGE(15),
+    REASON_PACKAGE_UPDATED(16),
     REASON_PERMISSION_CHANGE(8),
     REASON_SIGNALED(2),
     REASON_UNKNOWN(0),
-    REASON_USER_REQUESTED(0),
-    REASON_USER_STOPPED(1);
+    REASON_USER_REQUESTED(10),
+    REASON_USER_STOPPED(11);
 
     companion object {
         fun fromReason(reason: Int): CrashReason? {
